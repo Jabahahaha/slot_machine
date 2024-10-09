8efaadccfe02b5ff7910ba85d7b352ca48b60d46
Jabahahaha <misha.andreev3004@gmail.com>
2024-10-09 18:29:14 +0300

    1

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main(void) {
     int numO, numT, numTh;
     int jackWin;
     int yesNo;
-    int spins; // Variable to store number of spins
+    int spins; 
 
     srand(time(NULL)); // Initialize random number generator
     printf("Welcome to the Slot Machines! You start with $1000. The bet is $5.\n");
@@ -16,17 +16,14 @@ int main(void) {
     printf("\n");
 
     do {
-        // Ask the user how many spins they want to make
         printf("How many spins would you like to make? ");
         scanf("%d", &spins);
 
         for (int i = 0; i < spins; i++) {
-            // Generate random numbers for the spin
-            numO = rand() % 5 + 1;   // Random number between 1 and 5
-            numT = rand() % 6 + 1;   // Random number between 1 and 6
-            numTh = rand() % 4 + 1;  // Random number between 1 and 4
+            numO = rand() % 6 + 1;   
+            numT = rand() % 6 + 1;   
+            numTh = rand() % 6 + 1; 
 
-            // Deduct the bet for the current spin
             cash -= bet;
 
             printf("Current Balance = $%4d    ", cash);
@@ -34,23 +31,20 @@ int main(void) {
 
             // Jackpot condition: all three numbers match
             if (numO == numT && numT == numTh) {
-                jackWin = 100;
+                jackWin = bet * 30;
                 cash += jackWin;
                 printf("Jackpot! You win $%d!\n", jackWin);
             }
-            // No matches: lose the bet
             else {
                 printf("Sorry, no matches.\n");
             }
 
-            // Stop the game if the user runs out of money
             if (cash < bet) {
                 printf("You're out of money! Game over.\n");
                 return 0;
             }
         }
 
-        // Ask the user if they want to continue
         printf("Would you like to spin again (Yes=1, No=0)? ");
         scanf("%d", &yesNo);
 
